422ffa5339c98b802f26609104c745ba43cf6ea4
Saole94 <saole1994@yandex.ru>
2024-07-30 21:01:53 +0300

    Скрипты готовые
    Revert "Скрипты готовые"
    
    This reverts commit e6d5d5d10e463ef06228205a7b980f7061be014c.
    
    Скрипты итоговые

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+    const container = document.querySelector('.content');
+    const cardContainer = container.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
+    function createCard(item, onDeleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
+
+    deleteButton.addEventListener('click', onDeleteCard);
+
+    return cardElement
+    }
 
 // @todo: Функция удаления карточки
+    function deleteCard(evt){
+    evt.target.closest('.places__item').remove();
+}
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    const itemCard = createCard(item, deleteCard);
+    cardContainer.append(itemCard);
+   });
